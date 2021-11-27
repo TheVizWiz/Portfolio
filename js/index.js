7f8c6fb990a63372600978c5d3976b92e2f0ed08
Vibhu <vibhusiyer@gmail.com>
2021-11-26 21:05:20 -0500

    highlight work

--- js/index.js
+++ js/index.js
@@ -1,28 +1,36 @@
 
 
 $(function() {
-    let selectors = $(".titleJobs, .titleName, .titleSeparator");
+    let selectors = $(".titleJobs, .titleName");
 
     let time = 1000;
 
     selectors.css("pointer-events", "none");
     $(".titleName").fadeIn(time);
-    setTimeout(() => {
-        $(".titleSeparator").fadeIn(time);
-    }, time/3);
 
     for (let i = 1; i < 4; i++) {
         setTimeout(() => {
             $("#job" + i).hide().fadeIn(time);
-        } , time * 2 / 3 + i * 200);
+        } , time / 3 + i * time/5);
     }
 
     setTimeout(() => {
         selectors.css("transition-duration", ".1s").css("pointer-events", "all");
         $(".titleJobs").children().css("transition-duration", ".1s").css("pointer-events", "all");
-    },2*time);
+    },time );
 
 
-});
+}); //starter function
+
+
+
+addEventListener("click", (e) => {
+
+    console.log(document.getElementById("job3").innerHTML);
+    document.getElementById("job3").innerHTML = document.getElementById("job3").innerHTML.replace("d", "e");
+} )
+
+
+
 
 
