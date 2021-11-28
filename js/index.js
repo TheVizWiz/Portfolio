8f918138e11821ce59105ac09847e435a41b9427
TheVizWiz <vibhusiyer@gmail.com>
2021-11-28 03:08:39 -0500

    header bar setup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+let topProject;
+
 $(function () {
     let selectors = $(".titleJobs, .titleName");
 
@@ -55,12 +57,82 @@ $(function () {
         }
     }, 3000);
 
-
-
-
+    // topProject = $(".projectTitle")[0].boundingClientRect.top;
+    topProject = document.querySelector(".projectTitle").getBoundingClientRect().top;
+    zoomTopBar()
 }); //starter function
 
+addEventListener("resize", (e) => {
+    topProject = document.querySelector(".projectTitle").getBoundingClientRect().top;
+    zoomTopBar();
+})
+
+function zoomTopBar () {
+    if (document.documentElement.scrollTop > topProject) {
+        // $(".header").css("top", "-20px");
+        $(".logo").css("max-width", "50%").css("max-height", "50%");
+        $("#headerName").css("font-size", "3vmax")
+    } else {
+        // $(".header").css("top", "0");
+        $(".logo").css("max-width", "").css("max-height", "");
+        $("#headerName").css("font-size", "")
+
+        if (document.documentElement.getBoundingClientRect().width > 600) {
+            $(".header").css("padding", "")
+        }
+    }
+}
+// Element to move, time in ms to animate
+function scrollToTop(duration) {
+    console.log("scrolling")
+    var e = document.documentElement;
+    if (e.scrollTop === 0) {
+        var t = e.scrollTop;
+        ++e.scrollTop;
+        e = t + 1 === e.scrollTop-- ? e : document.body;
+    }
+    scrollToC(e, e.scrollTop, 0, duration);
+}
+
+// Element to move, element or px from, element or px to, time in ms to animate
+function scrollToC(element, from, to, duration) {
+    if (duration <= 0) return;
+    if (typeof from === "object") from = from.offsetTop;
+    if (typeof to === "object") to = to.offsetTop;
+
+    scrollToX(element, from, to, 0, 1 / duration, 20, easeOutCuaic);
+}
+
+function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+    if (t01 < 0 || t01 > 1 || speed <= 0) {
+        element.scrollTop = xTo;
+        return;
+    }
+    element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+    t01 += speed * step;
+
+    setTimeout(function() {
+        scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+    }, step);
+}
+
+function easeOutCuaic(t) {
+    t--;
+    return t * t * t + 1;
+}
+
+function rotateHeaderName() {
+    var el = document.getElementById('headerName');
+    el.style.animation = 'none';
+    el.offsetHeight; /* trigger reflow */
+    el.style.animation = null;
+}
 
 
 
 
+
+
+
+
+addEventListener("scroll", (e) => zoomTopBar())
